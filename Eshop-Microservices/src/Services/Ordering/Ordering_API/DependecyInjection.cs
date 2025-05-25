a9335238f4f9309438259b918a3dc65879df0f62
tshepangkagiso <tshepangkagisomashigo8@gmail.com>
2025-05-25 22:30:37 +0200

    Ordering_API version 1 complete

--- Eshop-Microservices/src/Services/Ordering/Ordering_API/DependecyInjection.cs
+++ Eshop-Microservices/src/Services/Ordering/Ordering_API/DependecyInjection.cs
@@ -4,12 +4,24 @@ public static class DependecyInjection
 {
     public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddCarter();
 
+        services.AddExceptionHandler<CustomExceptionHandler>();
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
+        app.MapCarter();
+
+        app.UseExceptionHandler(options => { });
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
 
         return app;
     }
